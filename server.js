b742d2c9d7271cde505c5c0292c026e8e96b23e5
RobsonMT <rbsndev3@gmail.com>
2022-01-25 18:23:53 -0300

    feat: db animes

---
+++ server.js
@@ -0,0 +1,24 @@
+const cors = require("cors");
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+const port = process.env.PORT || 3333;
+
+app.db = router.db;
+
+const rules = auth.rewriter({
+  "/users*": "/600/users$1",
+  "/mylist*": "/600/mylist$1",
+  "/comments*": "/660/comments$1",
+  "/animes*": "/664/animes$1",
+});
+
+app.use(cors());
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(port);
+
+console.log("Server is running on port:", port);
